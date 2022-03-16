6eafb682cfc6a869cf50eb9e345b53dc272e8aba
Pan-Grayza <zagraystepan@gmail.com>
2022-03-16 17:15:41 +0100

    Tutorial Chat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCne-GhR0yeOK4iVtDfVb0HDcBNb82g2Xg",
+    authDomain: "chat-react-3e87b.firebaseapp.com",
+    projectId: "chat-react-3e87b",
+    storageBucket: "chat-react-3e87b.appspot.com",
+    messagingSenderId: "478881601222",
+    appId: "1:478881601222:web:bfceb8f2e93f5e413aa9c7",
+    measurementId: "G-M82SD1TJ9B"
+    }
+);
+
+export const Context = createContext(null)
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Context.Provider value={{
+        firebase,
+        auth,
+        firestore
+    }}>
+        <App />
+    </Context.Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
