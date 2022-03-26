d364070b8c7baf37499656349a9da98da0eca80c
Pan-Grayza <zagraystepan@gmail.com>
2022-03-26 19:40:21 +0100

    Chat list fixed

--- src/components/Sidebar/ListItem/ListItem.jsx
+++ src/components/Sidebar/ListItem/ListItem.jsx
@@ -1,42 +1,35 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import classes from "./ListItem.module.css"
 
 const ListItem = ({ src, checked = false, h2 = "", p = "", id = null }) => {
     const [currentChat, setCurrentChat] = useState()
-    const li = document.querySelector(`#li_${id}`)
-    const ul = document.querySelector("#list_of_chats")
-    const input = document.querySelector(`#chat_checkbox_${id}`)
-    const span = document.querySelector(`#activate_tag_${id}`)
 
-    const handleChange = (e) => {
-        input.addEventListener("click", (e) => {
-            const allLi = ul.getElementsByTagName("li")
-            //
-            Array.from(allLi).forEach((li) => {
-                li.classList.remove("activated_li")
-            })
-            const target = e.target
-            if (target.checked) {
-                li.classList.add("activated_li")
-                span.classList.add("activated_tag")
-
-                setCurrentChat(target.value)
-            }
+    const HandleClick = (e) => {
+        const li = document.querySelector(`#li_${id}`)
+        const ul = document.querySelector("#list_of_chats")
+        const span = document.querySelector(`#activate_tag_${id}`)
+        const allLi = ul.getElementsByTagName("li")
+        const allSpans = ul.getElementsByTagName("span")
+        //
+        Array.from(allLi).forEach((li) => {
+            li.classList.remove("activated_li")
         })
+        Array.from(allSpans).forEach((span) => {
+            span.classList.remove("activated_tag")
+        })
+        const target = e.target
+        if (target) {
+            li.classList.add("activated_li")
+            span.classList.add("activated_tag")
+
+            setCurrentChat(id)
+        }
     }
 
     return (
         <li className={classes.list_item} id={`li_${id}`}>
-            <span id={"activate_tag_" + id}></span>
-            <input
-                type="radio"
-                className={classes.list_checkbox}
-                name="chat"
-                checked={currentChat == id}
-                id={"chat_checkbox_" + id}
-                onChange={handleChange}
-                value={id}
-            ></input>
+            <button className={classes.btn} onClick={HandleClick}></button>
+            <span className={classes.span_tag} id={"activate_tag_" + id}></span>
             <img className={classes.circle} src={src} alt="" />
             <div>
                 <h2>{h2}</h2>
