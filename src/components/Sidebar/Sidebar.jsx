e27a3c96718226479d8efc6a92c288a2eb30eaec
Pan-Grayza <zagraystepan@gmail.com>
2022-03-29 23:35:21 +0200

    Somehow done sidebar UX upgrade for mobile

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import classes from "./Sidebar.module.css"
 
 //Components
-import UserInfo from "../UserInfo/UserInfo"
 import ListItem from "./ListItem/ListItem"
 
 //Images
@@ -10,7 +9,7 @@ import img from "../../images/Earth.webp"
 
 const Sidebar = () => {
     return (
-        <div className={classes.sidebar}>
+        <div id="sidebar" className={classes.sidebar}>
             <div className={classes.list_of_chats} id="list_of_chats">
                 <ListItem src={img} h2={"Global"} id={1} />
 
