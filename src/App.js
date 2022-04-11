629f96e5d5c267e261491a90e5c31ff3b18126dd
Pan-Grayza <zagraystepan@gmail.com>
2022-04-11 12:58:40 +0100

    trying to deploy the site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, lazy, Suspense, useEffect } from "react"
+import React, { lazy, Suspense } from "react"
 import "./App.css"
 import classes from "./SignUpPage.module.css"
 //UI
--- src/components/Sidebar/ListItem/ListItem.jsx
+++ src/components/Sidebar/ListItem/ListItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import classes from "./ListItem.module.css"
 
 const ListItem = ({
